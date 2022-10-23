d2250394df8f59bb36bd77e16782f20980ff28d3
jchwenger <34098722+jchwenger@users.noreply.github.com>
2022-10-23 17:29:45 +0100

    process | increment

--- process.py
+++ process.py
@@ -17,7 +17,7 @@ def main(args):
 
     # Access all PNG files in directory
     all_images = [x for x in os.listdir(args.dir) if not x.startswith(".")]
-    N = len(all_images) - args.chunksize
+    N = len(all_images)
 
     # Assuming all images are the same size, get dimensions of first image
     dummy = np.array(Image.open(os.path.join(args.dir, all_images[0])).convert("RGB"))
@@ -34,7 +34,6 @@ def main(args):
         tmp_img = Image.fromarray(arr, mode="RGB")
         tmp_img.save(os.path.join(results_dir, name))
 
-
     def save_bulk(data):
         for arr, name in data:
             save_to_tiff(arr, name)
@@ -44,15 +43,20 @@ def main(args):
         print(f"creating results directory {results_dir}")
         os.mkdir(results_dir)
 
-    # Build up average pixel intensities, casting each image as an array of floats
-    for i in range(N):
-        print(f"{i}/{N}")
+    i = 0
+    print(f"chunksize: {args.chunksize} | increment: {args.increment}")
+    while i < N - args.chunksize:
+        if args.increment > 0:
+            print(f"{i}/{N} | chunksize: {args.chunksize}")
+        else:
+            print(f"{i}/{N}")
         avg_img, min_img, max_img = initialize_arrays()
-        subset = all_images[i:i+args.chunksize]
+        subset = all_images[i : i + args.chunksize]
         for j, img_filename in enumerate(subset):
             print(f"{j}/{len(subset)}\r", end="")
             current_img = np.array(
-                Image.open(os.path.join(args.dir, img_filename)).convert("RGB"), dtype=np.uint8
+                Image.open(os.path.join(args.dir, img_filename)).convert("RGB"),
+                dtype=np.uint8,
             )
             avg_img += current_img
             min_img = np.where(current_img <= min_img, current_img, min_img)
@@ -62,13 +66,16 @@ def main(args):
         # Round values in array and cast as 8-bit integer
         avg_img = np.array(np.round(avg_img), dtype=np.uint8)
 
-        save_bulk((
-            [avg_img, f"average.{i}.tiff"],
-            [min_img, f"min.{i}.tiff"],
-            [max_img, f"max.{i}.tiff"],
-
-        ))
+        save_bulk(
+            (
+                [avg_img, f"average.{i}.tiff"],
+                [min_img, f"min.{i}.tiff"],
+                [max_img, f"max.{i}.tiff"],
+            )
+        )
         print()
+        i += 1
+        args.chunksize += args.increment
 
 
 if __name__ == "__main__":
@@ -80,9 +87,25 @@ if __name__ == "__main__":
     )
 
     parser.add_argument(
-        "--chunksize", "-c", type=int, default=90, help="""
-        The chunk size on which the sliding calculation will be performed
-        """
+        "--chunksize",
+        "-c",
+        type=int,
+        default=90,
+        help="""
+        The chunk size on which the sliding calculation will be performed.
+        Default: 90.
+        """,
+    )
+
+    parser.add_argument(
+        "--increment",
+        "-n",
+        type=int,
+        default=00,
+        help="""
+        The increment by which the chunksize is increased at each step.
+        Default: 0.
+        """,
     )
 
     args = parser.parse_args()
